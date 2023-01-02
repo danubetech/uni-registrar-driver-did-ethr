b04b833481d62462a0d12fe2412bb399b2abf5a8
Markus Sabadello <markus@danubetech.com>
2023-01-02 21:30:07 +0100

    fix: Fix public key name for did:pkh

--- utils/requestUtils.ts
+++ utils/requestUtils.ts
@@ -12,7 +12,7 @@ export default {
             if (! didDocument.verificationMethod[0].publicKeyHex) throw "Verification method must have property 'publicKeyHex'";
             return didDocument.verificationMethod[0].publicKeyHex;
         } else if ('did:pkh' === provider) {
-            if ("#controllerKey" !== didDocument?.verificationMethod[0]?.id) throw "DID document must have exactly one verification method with id '#controllerKey'.";
+            if ("#blockchainAccountIdKey" !== didDocument?.verificationMethod[0]?.id) throw "DID document must have exactly one verification method with id '#blockchainAccountIdKey'.";
             if ("EcdsaSecp256k1VerificationKey2019" !== didDocument?.verificationMethod[0]?.type) throw "Verification method must be of type 'EcdsaSecp256k1VerificationKey2019'";
             if (! didDocument.verificationMethod[0].publicKeyHex) throw "Verification method must have property 'publicKeyHex'";
             return didDocument.verificationMethod[0].publicKeyHex;
