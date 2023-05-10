dc0c47ebd7660bba4ea124d04369b7640f05e581
Markus Sabadello <markus@danubetech.com>
2023-05-10 22:32:24 +0200

    Disable cheqd temporarily.

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import http from 'http';
 import express from 'express';
 import DidEthrController from './controllers/DidEthrController.js';
 import DidPkhController from './controllers/DidPkhController.js';
-import DidCheqdController from './controllers/DidCheqdController.js';
+//import DidCheqdController from './controllers/DidCheqdController.js';
 
 const serverPort = 9080;
 
@@ -25,9 +25,9 @@ app.post('/ethr/1.0/deactivate', DidEthrController.deactivate);
 app.post('/pkh/1.0/create', DidPkhController.create);
 app.post('/pkh/1.0/update', DidPkhController.update);
 app.post('/pkh/1.0/deactivate', DidPkhController.deactivate);
-app.post('/cheqd/1.0/create', DidCheqdController.create);
-app.post('/cheqd/1.0/update', DidCheqdController.update);
-app.post('/cheqd/1.0/deactivate', DidCheqdController.deactivate);
+//app.post('/cheqd/1.0/create', DidCheqdController.create);
+//app.post('/cheqd/1.0/update', DidCheqdController.update);
+//app.post('/cheqd/1.0/deactivate', DidCheqdController.deactivate);
 
 http.createServer(app).listen(serverPort, function () {
     console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
